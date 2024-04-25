1b91514d2fa3cd3e4947175fc7dc529f563916eb
wayne80361 <wayne80361@gmail.com>
2024-04-25 11:50:27 -0400

    import 3rd party API and create basic GUI

---
+++ yt.py
@@ -0,0 +1,26 @@
+import tkinter
+import customtkinter
+from pytube import YouTube
+from moviepy.editor import *
+
+
+# Basic GUI
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# App frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("Youtube Downloader")
+
+# Adding UI elements
+title = customtkinter.CTkLabel(app, text="Insert a youtube link")
+title.pack(padx=10, pady=10)
+
+# Link input
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
+link.pack()
+
+# Run App
+app.mainloop()
